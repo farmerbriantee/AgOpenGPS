edfd0075101ce1e23b4a9a55c5abfc61ea068fd9
Brian Tischler <yesgogit@gmail.com>
2017-04-16 20:41:46 -0600

    PID Adjust
    Change PID Gains on the fly

---
+++ AutoSteer/Steer.ino
@@ -0,0 +1,125 @@
+  #define LED_PIN 13 
+  
+  #define   DIR_PIN    12  //PB4
+  #define   PWM_PIN    11  //PB3
+  
+  #define WORKSW_PIN 4  //PD4
+  
+  #define RELAY1_PIN 5  //PD5
+  #define RELAY2_PIN 6  //PD6
+  #define RELAY3_PIN 7  //PD6
+  #define RELAY4_PIN 8  //PB0
+  #define RELAY5_PIN 9  //PB1
+  #define RELAY6_PIN 10  //PB2
+  #define RELAY7_PIN A4  //PC4
+  #define RELAY8_PIN A5  //PC5
+  
+  #define RAD2GRAD 57.2957795
+
+  byte relay = 0, workSwitch = 0, speeed = 0;
+  float distanceError = 0;
+  int headingError = 0;
+  byte pidSetting = 0;
+  
+  int steeringPosition = 0, steeringPosZero = 512;
+  
+  int pwmDrive = 0, drive = 0;;
+  float pValue = 0, iValue = 0, dValue = 0;
+ 
+  bool isHeaderFound = false, isDataReady = false;
+  int header = 0, tempHeader = 0, temp;
+
+  //PID variables
+  float Ko = 1.0f;  //overall gain
+  float Kp = 0.1f;  //proportional gain
+  float Ki = 0.008f;//integral gain
+  float Kd = 3.0f;  //derivative gain
+  
+  int   maxErr = 3000; //anti windup max
+  float lastError = 0, lastLastError = 0, integrated_error = 0, dError = 0;
+
+void loop()
+{
+  // header high/low, relay byte, speed byte, high distance, low distance, high delta, low delta, PID Settings byte
+  if (Serial.available() > 0 & !isHeaderFound) //find the header, 127H + 254L = 32766
+  {
+    int temp = Serial.read();
+    header = tempHeader << 8 | temp;               //high,low bytes to make int
+    tempHeader = temp;                             //save for next time
+    if (header == 32766) isHeaderFound = true;     //Do we have a match? 
+   }
+  
+  //Header has been found, presumably the next 6 bytes are the data
+  if (Serial.available()> 6 & isHeaderFound)
+  {
+        relay = Serial.read();   // read relay control from AgOpenGPS
+        speeed = Serial.read()>>2;  //single byte
+        distanceError = (float)(Serial.read() << 8 | Serial.read());   //high,low bytes     
+        headingError = (float)(Serial.read() << 8 | Serial.read());     //high low bytes 
+        pidSetting = (byte)Serial.read();
+
+        if (pidSetting == 0) {}
+        else {
+          switch(pidSetting){
+            case 3:{ Kp +=0.1; break; }
+            case 2:{ Kp -=0.1; break; }
+            case 5:{ Ki +=0.001; break; }
+            case 4:{ Ki -=0.001; break; }    
+            case 11:{Kd +=0.5; break; }      
+            case 10:{Kd -=0.5; break; }      
+            case 17:{Ko +=0.1; break; }      
+            case 16:{Ko +=0.1; break; }         
+          }
+          
+        }
+        isHeaderFound = false;
+        isDataReady = true;              
+  }        
+       
+  if (isDataReady)
+  {
+    isDataReady = false;
+    workSwitch = digitalRead(WORKSW_PIN);  // read work switch
+    SetRelays();
+    steeringPosition = ( analogRead(A0) - steeringPosZero);   //read the steering position sensor
+    delay(2);
+  
+    if (distanceError == 32020 | distanceError == 32000       //auto Steer is off if 32020, invalid if 32000
+          | abs(distanceError) > 3000 | headingError > 4000)  // greater then 2 meters off, steep angle > .4 rads
+    {
+      pwmDrive = 0; //turn off steering motor
+      motorDrive();
+      
+      Serial.println(steeringPosition);
+      //bitClear(PINB, 5); 
+    }
+    
+    else          //valid spot to turn on autosteer
+    {
+      bitSet(PINB, 5);                                              //turn LED on showing in headingDelta loop      
+      calcDistancePID();                                            //the distance loop
+    
+      if (distanceError > 0 )       //on the right side of line
+      {                
+      }  
+      else                          //on the left side. 
+      {      
+      }       
+  
+      Serial.print(Kp);
+      Serial.print(" ");    
+      Serial.print(Ki*100);
+      Serial.print(" ");    
+      Serial.print(Kd);
+      Serial.print("  ");    
+      Serial.print(pValue);
+      Serial.print(" ");    
+      Serial.print(iValue);
+      Serial.print(" ");    
+      Serial.print(dValue);
+      Serial.print(" ");    
+      Serial.println(pwmDrive);
+    } 
+  }
+  	
+}
