14c94a38621723d232bf86d7bc76defb177983d4
Brian Tischler <yesgogit@gmail.com>
2017-04-23 16:53:59 -0600

    v 2.13 Offsets PID Larger GUI
    Made GUI larger for visiibility, Offset now works with guidance too, see
    changelog for additions

--- Steer/Steer.ino
+++ Steer/Steer.ino
@@ -21,12 +21,11 @@
   int headingError = 0;
   byte pidSetting = 0;
   
-  int steeringPosition = 0, steeringPosZero = 512;
-  
-  int pwmDrive = 0, drive = 0;;
+  int steeringPosition = 0, steeringPosZero = 512;  
+  int pwmDrive = 0, drive = 0, pwmDisplay = 0;
   float pValue = 0, iValue = 0, dValue = 0;
  
-  bool isHeaderFound = false, isDataReady = false;
+  bool isDataFound = false, isSettingFound = false;
   int header = 0, tempHeader = 0, temp;
 
   //PID variables
@@ -35,91 +34,92 @@
   float Ki = 0.008f;//integral gain
   float Kd = 3.0f;  //derivative gain
   
-  int   maxErr = 3000; //anti windup max
+  int   maxIntErr = 3000; //anti windup max  
   float lastError = 0, lastLastError = 0, integrated_error = 0, dError = 0;
 
 void loop()
 {
   // header high/low, relay byte, speed byte, high distance, low distance, high delta, low delta, PID Settings byte
-  if (Serial.available() > 0 & !isHeaderFound) //find the header, 127H + 254L = 32766
+  if (Serial.available() > 0 && !isDataFound && !isSettingFound) //find the header, 127H + 254L = 32766
   {
     int temp = Serial.read();
     header = tempHeader << 8 | temp;               //high,low bytes to make int
     tempHeader = temp;                             //save for next time
-    if (header == 32766) isHeaderFound = true;     //Do we have a match? 
+    if (header == 32766) isDataFound = true;     //Do we have a match? 
+    if (header == 32764) isSettingFound = true;     //Do we have a match? 
    }
   
   //Header has been found, presumably the next 6 bytes are the data
-  if (Serial.available()> 6 & isHeaderFound)
-  {
-        relay = Serial.read();   // read relay control from AgOpenGPS
-        speeed = Serial.read()>>2;  //single byte
-        distanceError = (float)(Serial.read() << 8 | Serial.read());   //high,low bytes     
-        headingError = (float)(Serial.read() << 8 | Serial.read());     //high low bytes 
-        pidSetting = (byte)Serial.read();
-
-        if (pidSetting == 0) {}
-        else {
-          switch(pidSetting){
-            case 3:{ Kp +=0.1; break; }
-            case 2:{ Kp -=0.1; break; }
-            case 5:{ Ki +=0.001; break; }
-            case 4:{ Ki -=0.001; break; }    
-            case 11:{Kd +=0.5; break; }      
-            case 10:{Kd -=0.5; break; }      
-            case 17:{Ko +=0.1; break; }      
-            case 16:{Ko -=0.1; break; }         
-          }
-          
-        }
-        isHeaderFound = false;
-        isDataReady = true;              
-  }        
-       
-  if (isDataReady)
-  {
-    isDataReady = false;
+  if (Serial.available()> 5 && isDataFound)
+  {  
+    relay = Serial.read();   // read relay control from AgOpenGPS
+    speeed = Serial.read()>>2;  //single byte
+    distanceError = (float)(Serial.read() << 8 | Serial.read());   //high,low bytes     
+    headingError = (float)(Serial.read() << 8 | Serial.read());     //high low bytes 
+    
+    isDataFound = false;    
     workSwitch = digitalRead(WORKSW_PIN);  // read work switch
     SetRelays();
     steeringPosition = ( analogRead(A0) - steeringPosZero);   //read the steering position sensor
     delay(2);
   
     if (distanceError == 32020 | distanceError == 32000       //auto Steer is off if 32020, invalid if 32000
-          | abs(distanceError) > 3000 | headingError > 4000)  // greater then 2 meters off, steep angle > .4 rads
+          | abs(distanceError) > 3000 | headingError > 4000  // greater then 2 meters off, steep angle > .4 rads
+          | speeed < 3)                                       //Speed is too slow
     {
       pwmDrive = 0; //turn off steering motor
       motorDrive();
+      bitClear(PORTD, 6); //Digital Pin 6
       
-      Serial.println(steeringPosition);
+      Serial.print(steeringPosition);
+      Serial.print(" ");    
+      Serial.print(Kp);
+      Serial.print(" ");    
+      Serial.print(Ki,4);
+      Serial.print(" ");    
+      Serial.print(Kd);
+      Serial.print(" ");    
+      Serial.print(Ko);
+      Serial.print(" ");    
+      Serial.println(maxIntErr);
       //bitClear(PINB, 5); 
     }
     
     else          //valid spot to turn on autosteer
     {
-      bitSet(PINB, 5);                                              //turn LED on showing in headingDelta loop      
-      calcDistancePID();                                            //the distance loop
-    
+      bitSet(PORTD, 6);             //Digital Pin 6
+      bitSet(PINB, 5);              //turn LED on showing in headingDelta loop      
+      calcDistancePID();            //the distance loop                                                                                    
+      
       if (distanceError > 0 )       //on the right side of line
       {                
       }  
       else                          //on the left side. 
       {      
       }       
-  
-      Serial.print(Kp);
+      motorDrive();                 //out to motors the pwm value
+      
+      Serial.print(steeringPosition);
       Serial.print(" ");    
-      Serial.print(Ki*100);
-      Serial.print(" ");    
-      Serial.print(Kd);
-      Serial.print("  ");    
       Serial.print(pValue);
       Serial.print(" ");    
       Serial.print(iValue);
       Serial.print(" ");    
       Serial.print(dValue);
       Serial.print(" ");    
-      Serial.println(pwmDrive);
+      Serial.println(pwmDisplay);
     } 
   }
-  	
+  
+  //Header has been found, presumably the next 5 bytes are the settings
+  if (Serial.available() > 4 && isSettingFound)
+  {  
+    isSettingFound = false;
+    Kp = (float)Serial.read() * 0.01;   // read Kp from AgOpenGPS
+    Ki = (float)Serial.read() * 0.001;   // read Ki from AgOpenGPS
+    Kd = (float)Serial.read() * 0.1;   // read Kd from AgOpenGPS
+    Ko = (float)Serial.read() * 0.1;   // read Ko from AgOpenGPS
+    maxIntErr = Serial.read() * 100;  
+  }
+   	
 }
