da26a21ec8a50247f082b69c1115189918dc3570
Brian Tischler <yesgogit@gmail.com>
2017-07-19 09:22:04 -0600

    Autosteer Update
    Cahnges to Arduino code and few changes to AgOpenGPS

---
+++ AutoSteer/PID.ino
@@ -0,0 +1,45 @@
+void calcSteeringPID(void) {
+  
+  //proportional
+  pValue = Kp * headingError;
+  
+  //Integral
+
+  integrated_error = integrated_error + headingError;
+  if (integrated_error > maxIntErr) integrated_error = maxIntErr;
+  if (integrated_error < -maxIntErr) integrated_error = -maxIntErr;
+
+  //once it passes near center zero out the integrator error
+  //if (abs(headingError) < 2) integrated_error = 0;
+  iValue = Ki * integrated_error;
+
+  //Derivative
+  dError = headingError - lastError;
+  dValue = Kd * (dError);
+  
+  //save history of errors
+  lastLastError = lastError;
+  lastError = headingError;
+
+  drive = (pValue + iValue + dValue)*Ko;
+  pwmDrive = int(constrain(drive, -255, 255));
+
+  //add throttle factor so no delay from motor resistance.
+  if (pwmDrive < 0 & pwmDrive > (-255 + minPWMValue)) pwmDrive = pwmDrive - minPWMValue;
+  else if (pwmDrive > 0 & pwmDrive < (255 - minPWMValue)) pwmDrive = pwmDrive + minPWMValue;
+
+ }
+
+ void motorDrive(void) 
+  {
+    pwmDisplay = pwmDrive;
+    if (pwmDrive > 0) bitSet(PORTB, 4);  //set the correct direction
+    else   
+    {
+      bitClear(PORTB, 4); 
+      pwmDrive = -1 * pwmDrive;  
+   }
+    analogWrite(PWM_PIN, pwmDrive);
+  }
+
+
