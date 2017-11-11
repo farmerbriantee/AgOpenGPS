1c81d6d358966ecde3f6f365edcd45ae9f2b184c
Brian Tischler <yesgogit@gmail.com>
2017-11-11 12:43:44 -0700

    v2.2
    YouTurn, manual turning, Spray control, new menu

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 
 # AgOpenGPS
 
-This project is for my personal use only, and has no commercial value whatsoever. This software is not for sale, is incomplete, is in development to show concepts only and is mostly non functional. Any use of this software is not recommended and is intended for simulation only.
+*** Important ****
+
+This software is for discussion and learning of Precision Ag
+algorithms and techniques. It is not in any way to be used on
+any physical equipment and is meant for use by a simulator only.
+
+Any use of AgOpenGPS on real physical equipment is not allowed
+and is not recommended in any way, neither off road or on road. 
+
+It is completely unsafe and has no safety limits built in or implied.
+
+AgOpenGPS is strictly an example of C# programming for
+demonstration purposes only and shall not be used for any other purpose.
+
 
 Ag Precision Mapping and Section Control Software
 
---
+++ SharpGL - Dev/OGL/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace OGL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.DrawFPS = false;
+            this.openGLControl1.Location = new System.Drawing.Point(53, 35);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(450, 326);
+            this.openGLControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 451);
+            this.Controls.Add(this.openGLControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl1;
+
+
+    }
+}
+
