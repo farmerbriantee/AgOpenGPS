a28b5a8c83ecf7db688bcec9bcfd901e5d55eb65
Brian Tischler <yesgogit@gmail.com>
2019-08-31 14:43:53 -0600

    Fix last Resx
    Missed one

--- SourceCode/GPS/Forms/FormSaveOrNot.Designer.cs
+++ SourceCode/GPS/Forms/FormSaveOrNot.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSaveOrNot));
             this.lblQuestion = new System.Windows.Forms.Label();
             this.btnReturn = new System.Windows.Forms.Button();
             this.btnOk = new System.Windows.Forms.Button();
@@ -37,40 +36,61 @@
             // 
             // lblQuestion
             // 
-            resources.ApplyResources(this.lblQuestion, "lblQuestion");
+            this.lblQuestion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Tahoma", 15.75F);
+            this.lblQuestion.Location = new System.Drawing.Point(59, 21);
             this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(0, 25);
+            this.lblQuestion.TabIndex = 3;
             // 
             // btnReturn
             // 
-            resources.ApplyResources(this.btnReturn, "btnReturn");
+            this.btnReturn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnReturn.Font = new System.Drawing.Font("Tahoma", 15.75F);
             this.btnReturn.Image = global::AgOpenGPS.Properties.Resources.back_button;
+            this.btnReturn.Location = new System.Drawing.Point(32, 64);
             this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(127, 85);
+            this.btnReturn.TabIndex = 5;
             this.btnReturn.UseVisualStyleBackColor = true;
             this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
             // 
             // btnOk
             // 
-            resources.ApplyResources(this.btnOk, "btnOk");
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnOk.Font = new System.Drawing.Font("Tahoma", 15.75F);
             this.btnOk.Image = global::AgOpenGPS.Properties.Resources.FileSave;
+            this.btnOk.Location = new System.Drawing.Point(234, 64);
             this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(127, 85);
+            this.btnOk.TabIndex = 0;
             this.btnOk.UseVisualStyleBackColor = true;
             this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
             // label2
             // 
-            resources.ApplyResources(this.label2, "label2");
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F);
+            this.label2.Location = new System.Drawing.Point(220, 21);
             this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 7;
             // 
             // FormSaveOrNot
             // 
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(403, 170);
             this.ControlBox = false;
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.lblQuestion);
             this.Controls.Add(this.btnOk);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F);
             this.Name = "FormSaveOrNot";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.ResumeLayout(false);
             this.PerformLayout();
 
