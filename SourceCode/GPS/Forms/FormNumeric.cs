8788ad8c0a0a20b06443870dc5e33fec93dd215b
Brian Tischler <yesgogit@gmail.com>
2019-10-08 10:24:58 -0600

    Keypad fix
    Any key clears display on first push, comma decimal sorted

--- SourceCode/GPS/Forms/FormNumeric.cs
+++ SourceCode/GPS/Forms/FormNumeric.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Globalization;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace AgOpenGPS
@@ -9,6 +10,7 @@ namespace AgOpenGPS
     {
         private readonly double max;
         private readonly double min;
+        private bool isFirstKey;
 
         public double ReturnValue { get; set; }
 
@@ -20,6 +22,8 @@ namespace AgOpenGPS
 
             //fill in the display
             tboxNumber.Text = currentValue.ToString();
+
+            isFirstKey = true;
         }
 
         private void FormNumeric_Load(object sender, EventArgs e)
@@ -33,6 +37,13 @@ namespace AgOpenGPS
 
         private void RegisterKeypad1_ButtonPressed(object sender, KeyPressEventArgs e)
         {
+
+            if (isFirstKey)
+            {
+                tboxNumber.Text = "";
+                isFirstKey = false;
+            }
+
             //clear the error as user entered new values
             if (tboxNumber.Text == "Error")
             {
@@ -60,20 +71,20 @@ namespace AgOpenGPS
             else if (e.KeyChar == '.')
             {
                 //does it already have a decimal?
-                if (!tboxNumber.Text.Contains("."))
+                if (!tboxNumber.Text.Contains(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator))
                 {
-                    tboxNumber.Text += e.KeyChar;
+                    tboxNumber.Text += Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
 
                     //if decimal is first char, prefix with a zero
-                    if (tboxNumber.Text.IndexOf(".") == 0)
+                    if (tboxNumber.Text.IndexOf(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator) == 0)
                     {
                         tboxNumber.Text = "0" + tboxNumber.Text;
                     }
 
                     //neg sign then added a decimal, insert a 0 
-                    if (tboxNumber.Text.IndexOf("-") == 0 && tboxNumber.Text.IndexOf(".") == 1)
+                    if (tboxNumber.Text.IndexOf("-") == 0 && tboxNumber.Text.IndexOf(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator) == 1)
                     {
-                        tboxNumber.Text = "-0.";
+                        tboxNumber.Text = "-0" + Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
                     }
                 }
             }
@@ -117,7 +128,7 @@ namespace AgOpenGPS
                 if (tboxNumber.Text == "") return;
 
                 //culture invariant parse to double
-                double tryNumber = double.Parse(tboxNumber.Text, CultureInfo.InvariantCulture);
+                double tryNumber = double.Parse(tboxNumber.Text, CultureInfo.CurrentCulture);
 
                 //test if above or below min/max
                 if (tryNumber < min)
@@ -143,7 +154,7 @@ namespace AgOpenGPS
         private void BtnDistanceUp_MouseDown(object sender, MouseEventArgs e)
         {
             if (tboxNumber.Text == "" || tboxNumber.Text == "-" || tboxNumber.Text == "Error") tboxNumber.Text = "0";
-            double tryNumber = double.Parse(tboxNumber.Text, CultureInfo.InvariantCulture);
+            double tryNumber = double.Parse(tboxNumber.Text, CultureInfo.CurrentCulture);
 
 
             tryNumber++;
@@ -151,17 +162,21 @@ namespace AgOpenGPS
             if (tryNumber > max) tryNumber = max;
 
             tboxNumber.Text = tryNumber.ToString();
+
+            isFirstKey = false;
         }
 
         private void BtnDistanceDn_MouseDown(object sender, MouseEventArgs e)
         {
             if (tboxNumber.Text == "" || tboxNumber.Text == "-" || tboxNumber.Text == "Error") tboxNumber.Text = "0";
-            double tryNumber = double.Parse(tboxNumber.Text, CultureInfo.InvariantCulture);
+            double tryNumber = double.Parse(tboxNumber.Text, CultureInfo.CurrentCulture);
 
             tryNumber--;
             if (tryNumber < min) tryNumber = min;
 
             tboxNumber.Text = tryNumber.ToString();
+
+            isFirstKey = false;
         }
     }
 }
