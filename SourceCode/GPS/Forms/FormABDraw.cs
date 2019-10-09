5b97b48d9d32660dff6cee35f5faaba551d88da2
Daniel P <42768553+P00qwerty@users.noreply.github.com>
2019-10-09 11:19:16 +0200

    Update FormABDraw.cs

--- SourceCode/GPS/Forms/FormABDraw.cs
+++ SourceCode/GPS/Forms/FormABDraw.cs
@@ -382,21 +382,21 @@ namespace AgOpenGPS
         private void FormABDraw_Load(object sender, EventArgs e)
         {
 
-            int cnt = mf.bnd.bndArr[0].bndLine.Count;
+            int cnt = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine.Count;
             arr = new vec3[cnt*2];
 
             for (int i = 0; i < cnt; i++)
             {
-                arr[i].easting = mf.bnd.bndArr[0].bndLine[i].easting;
-                arr[i].northing = mf.bnd.bndArr[0].bndLine[i].northing;
-                arr[i].heading = mf.bnd.bndArr[0].bndLine[i].northing;
+                arr[i].easting = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine[i].easting;
+                arr[i].northing = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine[i].northing;
+                arr[i].heading = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine[i].northing;
             }
 
             for (int i = cnt; i < cnt*2; i++)
             {
-                arr[i].easting = mf.bnd.bndArr[0].bndLine[i-cnt].easting;
-                arr[i].northing = mf.bnd.bndArr[0].bndLine[i-cnt].northing;
-                arr[i].heading = mf.bnd.bndArr[0].bndLine[i-cnt].heading;
+                arr[i].easting = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine[i-cnt].easting;
+                arr[i].northing = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine[i-cnt].northing;
+                arr[i].heading = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine[i-cnt].heading;
             }
         }
 
@@ -573,13 +573,13 @@ namespace AgOpenGPS
                 }
 
                 //min max of the boundary
-                if (mf.bnd.bndArr[0].isSet)
+                if (mf.bnd.bndArr.Count > 0)
                 {
-                    int bndCnt = mf.bnd.bndArr[0].bndLine.Count;
+                    int bndCnt = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine.Count;
                     for (int i = 0; i < bndCnt; i++)
                     {
-                        double x = mf.bnd.bndArr[0].bndLine[i].easting;
-                        double y = mf.bnd.bndArr[0].bndLine[i].northing;
+                        double x = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine[i].easting;
+                        double y = mf.bnd.bndArr[mf.bnd.LastBoundary].bndLine[i].northing;
 
                         //also tally the max/min of field x and z
                         if (minFieldX > x) minFieldX = x;
