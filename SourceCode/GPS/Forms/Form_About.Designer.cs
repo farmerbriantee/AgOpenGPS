5d1c616ef70dbe7acf6327e47409d4c1d85c4dea
Brian Tischler <yesgogit@gmail.com>
2019-10-23 11:05:35 -0600

    Form About Fixed
    Youtube video example

--- SourceCode/GPS/Forms/Form_About.Designer.cs
+++ SourceCode/GPS/Forms/Form_About.Designer.cs
@@ -125,7 +125,7 @@
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.button1.Image = global::AgOpenGPS.Properties.Resources.OK64;
-            this.button1.Location = new System.Drawing.Point(328, 366);
+            this.button1.Location = new System.Drawing.Point(619, 376);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 70);
             this.button1.TabIndex = 10;
