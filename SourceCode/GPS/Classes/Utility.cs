5abbd9a9a6fab177c1ac405ebfcd54b1c850c878
Travis Martin <16525827+travistymartini@users.noreply.github.com>
2020-02-15 23:49:53 +0800

    Implement method to check and alter angles
    Check the value that is passed in, alter (if required) and return to
    caller. Use to ensure an angle is greater than or equal to zero
    (not negative) and less than or equal to TAU (2 * PI), in terms of
    radians.
    
    This check is useful to ensure "heading" is between the range specified
    above. For example, to correct the possible negative value that can be
    returned from the trigonometry functions in "System.Math": Atan, Atan2,
    Asin, Acos;
    
    The logic used can be found duplicated throughout the code when it is
    needed. Moving the logic into a method will increase code readability
    and is less error prone. This method should be used instead.

---
+++ SourceCode/GPS/Classes/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AgOpenGPS
+{
+    public static class
+    Utility
+    {
+        const double TAU = (2 * Math.PI);
+
+        public static double
+        CheckAngle(double angle)
+        {
+            for (; angle < 0D;) { angle += TAU; }
+
+            for (; angle > TAU;) { angle -= TAU; }
+
+            return angle;
+        }
+    }
+}
