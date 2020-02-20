cda8d1c4dfb57b538dedf6bc2e29d5c2be75632d
Travis Martin <16525827+travistymartini@users.noreply.github.com>
2020-02-20 11:37:27 +0800

    Implement mathematic geometric vectors
    Add a type to represent a geometric vector in two dimensions. These
    vectors can be declared using either: (cartesian) 'change of x' and
    'change of y' or (polar) 'magnitude' and 'direction'.
    
    The Vector2 constructor can not be called directly, instead, two methods
    are provided by the Vector2 type: 'Vector2.Cartesian' and
    'Vector.Polar'. Given appropriate inputs these methods do the necessary
    calculations and pass the resulting values to the Vector2 constructor,
    which in turn is returned to the caller.
    
    The dual defintion removes the need to convert between coordinate
    systems and the need to rememeber which system a particular vector was
    defined in terms of.

---
+++ SourceCode/GPS/Classes/Geometrics.cs
@@ -0,0 +1,89 @@
+using System;
+
+namespace AgOpenGPS
+{
+    public static class Geometrics
+    {
+        /*
+        *  Geometric vector defined in two dimensions.
+        *  Defined in terms of cartesian x,y change
+        *  and polar magnitude and direction.
+        */
+        public struct
+        Vector2
+        {
+            public readonly double x;
+            public readonly double y;
+            public readonly double magnitude;
+            public readonly double direction;
+
+            private
+            Vector2(double p_x, double p_y, double p_magnitude, double p_direction)
+            {
+                x = p_x;
+                y = p_y;
+                magnitude = p_magnitude;
+                direction = p_direction;
+            }
+
+            public static Vector2
+            Polar(double magnitude, double direction)
+            {
+                magnitude = Math.Abs(magnitude);
+
+                const double twoPI = 2 * Math.PI;
+                for (; direction < 0D;) { direction += twoPI; }
+                for (; direction > twoPI;) { direction -= twoPI; }
+
+                double x = (magnitude * Math.Cos(direction));
+                double y = (magnitude * Math.Sin(direction));
+
+                return new Vector2(x, y, magnitude, direction);
+            }
+
+            public static Vector2
+            Cartesian(double x, double y)
+            {
+                double magnitude =
+                Math.Abs(Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)));
+
+                double direction = Math.Atan2(y, x);
+                {
+                    const double twoPI = 2 * Math.PI;
+                    for (; direction < 0D;) { direction += twoPI; }
+                    for (; direction > twoPI;) { direction -= twoPI; }
+                }
+
+                return new Vector2(x, y, magnitude, direction);
+            }
+
+            public static Vector2 operator +
+            (Vector2 left, Vector2 right)
+            {
+                return Vector2.Cartesian
+                    (left.x + right.x, left.y + right.y);
+            }
+
+            public static Vector2 operator -
+            (Vector2 left, Vector2 right)
+            {
+                return Vector2.Cartesian
+                    (left.x + (-right.x), left.y + (-right.y));
+            }
+
+            public static Vector2
+            Reverse(Vector2 vector)
+            {
+                return Vector2.Cartesian
+                    (-(vector.x), -(vector.y));
+            }
+
+            public static Vector2
+            Scale(Vector2 vector, double scalar)
+            {
+                return Vector2.Polar
+                    (vector.magnitude * scalar, vector.direction);
+            }
+        }
+    }
+}
