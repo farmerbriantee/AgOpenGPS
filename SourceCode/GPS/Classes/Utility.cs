2f10849264110c91224661cb4ae3e1d8c4f94556
Travis Martin <16525827+travistymartini@users.noreply.github.com>
2020-03-03 22:09:16 +0800

    Implement methods to calculate relative angles
    Provided two angles, these methods will calculate the amount of
    angle between the two angles, from the first, to the second. The
    direction of calculation (and therefore the sign of the result) is
    detirmined by which method is used.
    
     * "RelativeAngle_L" will measure 'left' (anti-clockwise) from the first
       to the second angle, and will return a positive value.
     * "RelativeAngle_R" will measure 'right' (clockwise) from the first to
       the second angle, and will return a negative value.

--- SourceCode/GPS/Classes/Utility.cs
+++ SourceCode/GPS/Classes/Utility.cs
@@ -16,5 +16,41 @@ namespace AgOpenGPS
 
             return angle;
         }
+
+        public static double
+        RelativeAngle_L(double angle1, double angle2)
+        {
+            angle1 = CheckAngle(angle1);
+            angle2 = CheckAngle(angle2);
+
+            if (angle1 > angle2)
+            {
+                return (angle2 + TAU) - angle1;
+            }
+            else if (angle1 < angle2)
+            {
+                return angle2 - angle1;
+            }
+
+            return 0D;
+        }
+
+        public static double
+        RelativeAngle_R(double angle1, double angle2)
+        {
+            angle1 = CheckAngle(angle1);
+            angle2 = CheckAngle(angle2);
+
+            if (angle1 < angle2)
+            {
+                return -((angle1 + TAU) - angle2);
+            }
+            else if (angle1 > angle2)
+            {
+                return -(angle1 - angle2);
+            }
+
+            return 0D;
+        }
     }
 }
