a54ce9b1f50d03bdc9ef03d755d29f89d027e006
Ray Bear <ray.bear@iinet.net.au>
2020-03-31 15:32:56 +0800

    Added Keyboard
    Added Keyboard similar to Keypad

--- SourceCode/GPS/Properties/Settings.Designer.cs
+++ SourceCode/GPS/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace AgOpenGPS.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -1257,6 +1257,30 @@ namespace AgOpenGPS.Properties {
             }
             set {
                 this["setUDP_isInterAppOn"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool setIsKeyboardOn {
+            get {
+                return ((bool)(this["setIsKeyboardOn"]));
+            }
+            set {
+                this["setIsKeyboardOn"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool setIsKeypadOn {
+            get {
+                return ((bool)(this["setIsKeypadOn"]));
+            }
+            set {
+                this["setIsKeypadOn"] = value;
             }
         }
     }
