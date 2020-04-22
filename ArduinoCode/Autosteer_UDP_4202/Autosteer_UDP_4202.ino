472a1b370fb366687cc119d87decc474c78fd123
Brian Tischler <yesgogit@gmail.com>
2020-04-22 06:54:46 -0600

    Nano says "Setup Complete"
    Both udp and usb version say setup complete once going thru setup and waiting for AOG

--- ArduinoCode/Autosteer_UDP_4202/Autosteer_UDP_4202.ino
+++ ArduinoCode/Autosteer_UDP_4202/Autosteer_UDP_4202.ino
@@ -264,7 +264,7 @@ void setup()
         MMA1C.setRange(MMA_RANGE_2G);
         MMA1C.setHighPassFilter(false); 
       }
-      //else Serial.println("MMA init fails!!");
+      else Serial.println("MMA init fails!!");
   }
   else if (aogSettings.InclinometerInstalled == 3 )
   { 
@@ -277,7 +277,7 @@ void setup()
         MMA1D.setRange(MMA_RANGE_2G);
         MMA1D.setHighPassFilter(false); 
       }
-      //else Serial.println("MMA init fails!!");
+      else Serial.println("MMA init fails!!");
   }  
  
    if (ether.begin(sizeof Ethernet::buffer, mymac, CS_Pin) == 0)
@@ -295,6 +295,8 @@ void setup()
     
     //register udpSerialPrint() to port 8888
     ether.udpServerListenOnPort(&udpSteerRecv, 8888);
+
+    Serial.println("Setup complete, waiting for AgOpenGPS");
     
 }// End of Setup
 
