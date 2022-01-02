e81008e5b1be9bc930defd66d8da13da8c68fc02
Brian Tischler <yesgogit@gmail.com>
2022-01-02 07:27:44 -0600

    update uvt, remove Kalman

--- UVT/UVT/zGyro.ino
+++ UVT/UVT/zGyro.ino
@@ -9,18 +9,10 @@ void GyroHandler(uint32_t delta)
 	Wire.requestFrom(CMPS14_ADDRESS, 2);
 	while (Wire.available() < 2);
 
-	float gyro = int16_t(Wire.read() << 8 | Wire.read());
+	gyro = int16_t(Wire.read() << 8 | Wire.read());
 
 	//Complementary filter
-	gyroSum = 0.8 * gyroSum + 0.2 * gyro;
-
-	//Kalman filter
-	Pc = P + varProcess;
-	G = Pc / (Pc + varRoll);
-	P = (1 - G) * Pc;
-	Xp = kalGyro;
-	Zp = Xp;
-	kalGyro = (G * (gyroSum - Zp) + Xp);
+	gyroSum = 0.95 * gyroSum + 0.05 * gyro;
 
 	//save time to check for 10 msec
 	lastGyroTime = millis();
