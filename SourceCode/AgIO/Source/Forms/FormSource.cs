838d2a09bad8959d0286d89c3ce50ebaa2969640
Brian Tischler <yesgogit@gmail.com>
2022-04-21 11:56:47 -0600

    Go quiet when minimized manually

--- SourceCode/AgIO/Source/Forms/FormSource.cs
+++ SourceCode/AgIO/Source/Forms/FormSource.cs
@@ -60,8 +60,6 @@ namespace AgIO
                     itm = new ListViewItem(fieldNames);
                     lvLines.Items.Add(itm);
                 }
-
-
             }
             this.chName.Width = 250;
         }
@@ -177,7 +175,8 @@ namespace AgIO
             double num1 = longitude * (Math.PI / 180.0);
             double d2 = otherLatitude * (Math.PI / 180.0);
             double num2 = otherLongitude * (Math.PI / 180.0) - num1;
-            double d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
+            double d3 = Math.Pow(Math.Sin((d2 - d1) / 2.0), 2.0) + Math.Cos(d1) 
+                * Math.Cos(d2) * Math.Pow(Math.Sin(num2 / 2.0), 2.0);
 
             return 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
         }
