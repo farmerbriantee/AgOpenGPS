2b02a1c4dbb7ac806b8928bbf5e3640b5eedd086
BrianTee <yesgogit@gmail.com>
2024-02-14 22:48:55 -0700

    First and about

--- SourceCode/GPS/Properties/AssemblyInfo.cs
+++ SourceCode/GPS/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.8.5")]
-[assembly: AssemblyFileVersion("5.8.5")]
+[assembly: AssemblyVersion("6.0.3")]
+[assembly: AssemblyFileVersion("6.0.3")]
