3895b27028c752823e2d1122679acf5dd559b8e8
BrianTee <yesgogit@gmail.com>
2024-04-02 10:19:00 -0600

    Elevation log image

--- SourceCode/GPS/Properties/Resources.Designer.cs
+++ SourceCode/GPS/Properties/Resources.Designer.cs
@@ -1002,6 +1002,16 @@ namespace AgOpenGPS.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ConD_LogElevation {
+            get {
+                object obj = ResourceManager.GetObject("ConD_LogElevation", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ConD_LogNMEA {
             get {
                 object obj = ResourceManager.GetObject("ConD_LogNMEA", resourceCulture);
