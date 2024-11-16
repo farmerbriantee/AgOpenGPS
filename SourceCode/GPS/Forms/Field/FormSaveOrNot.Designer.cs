a0f5030e053e189848ad215ef9f22537a2d76fd8
BrianTee <yesgogit@gmail.com>
2024-11-16 08:43:11 -0700

    Fix shutdown exit cancel

--- SourceCode/GPS/Forms/Field/FormSaveOrNot.Designer.cs
+++ SourceCode/GPS/Forms/Field/FormSaveOrNot.Designer.cs
@@ -32,6 +32,7 @@
             this.lblQuestion = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -40,7 +41,6 @@
             this.btnOk = new System.Windows.Forms.Button();
             this.btnReturn = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label6 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,7 +49,7 @@
             this.lblQuestion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.lblQuestion.AutoSize = true;
             this.lblQuestion.Font = new System.Drawing.Font("Tahoma", 15.75F);
-            this.lblQuestion.Location = new System.Drawing.Point(85, 81);
+            this.lblQuestion.Location = new System.Drawing.Point(85, 113);
             this.lblQuestion.Name = "lblQuestion";
             this.lblQuestion.Size = new System.Drawing.Size(0, 25);
             this.lblQuestion.TabIndex = 3;
@@ -59,7 +59,7 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 15.75F);
-            this.label2.Location = new System.Drawing.Point(487, -60);
+            this.label2.Location = new System.Drawing.Point(487, -28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 25);
             this.label2.TabIndex = 7;
@@ -68,27 +68,38 @@
             // 
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.btnShutDown);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.btnOk);
             this.panel1.Controls.Add(this.btnReturn);
-            this.panel1.Location = new System.Drawing.Point(13, 17);
+            this.panel1.Location = new System.Drawing.Point(13, 18);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(705, 211);
+            this.panel1.Size = new System.Drawing.Size(705, 242);
             this.panel1.TabIndex = 260;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(424, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(196, 25);
+            this.label6.TabIndex = 267;
+            this.label6.Text = "Exit To Windows:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Tahoma", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(617, 145);
+            this.label1.Location = new System.Drawing.Point(613, 154);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 45);
+            this.label1.Size = new System.Drawing.Size(56, 58);
             this.label1.TabIndex = 266;
             this.label1.Text = "4";
             // 
@@ -97,7 +108,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(580, 15);
+            this.label4.Location = new System.Drawing.Point(26, 30);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(90, 19);
             this.label4.TabIndex = 265;
@@ -108,7 +119,7 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(51, 14);
+            this.label5.Location = new System.Drawing.Point(322, 30);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 19);
             this.label5.TabIndex = 262;
@@ -122,7 +133,7 @@
             this.btnShutDown.FlatAppearance.BorderSize = 0;
             this.btnShutDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnShutDown.Font = new System.Drawing.Font("Tahoma", 15.75F);
-            this.btnShutDown.Location = new System.Drawing.Point(582, 37);
+            this.btnShutDown.Location = new System.Drawing.Point(28, 52);
             this.btnShutDown.Name = "btnShutDown";
             this.btnShutDown.Size = new System.Drawing.Size(79, 79);
             this.btnShutDown.TabIndex = 263;
@@ -134,7 +145,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(358, 14);
+            this.label3.Location = new System.Drawing.Point(624, 30);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 19);
             this.label3.TabIndex = 260;
@@ -148,7 +159,7 @@
             this.btnOk.FlatAppearance.BorderSize = 0;
             this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnOk.Font = new System.Drawing.Font("Tahoma", 15.75F);
-            this.btnOk.Location = new System.Drawing.Point(333, 36);
+            this.btnOk.Location = new System.Drawing.Point(599, 52);
             this.btnOk.Name = "btnOk";
             this.btnOk.Size = new System.Drawing.Size(79, 79);
             this.btnOk.TabIndex = 0;
@@ -163,7 +174,7 @@
             this.btnReturn.FlatAppearance.BorderSize = 0;
             this.btnReturn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnReturn.Font = new System.Drawing.Font("Tahoma", 15.75F);
-            this.btnReturn.Location = new System.Drawing.Point(41, 36);
+            this.btnReturn.Location = new System.Drawing.Point(312, 52);
             this.btnReturn.Name = "btnReturn";
             this.btnReturn.Size = new System.Drawing.Size(79, 79);
             this.btnReturn.TabIndex = 5;
@@ -176,23 +187,12 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(372, 145);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(239, 45);
-            this.label6.TabIndex = 267;
-            this.label6.Text = "Cancel in:...";
-            // 
             // FormSaveOrNot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(731, 247);
+            this.ClientSize = new System.Drawing.Size(731, 279);
             this.ControlBox = false;
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblQuestion);
